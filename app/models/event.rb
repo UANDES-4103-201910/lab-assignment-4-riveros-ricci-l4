d919b9678f261da356b1e62ea3f942d8649b5bbb
Pedro <priccis@gmail.com>
2019-04-03 10:54:31 -0300

    Added required validations to Events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,20 @@
 class Event < ApplicationRecord
   belongs_to :event_venue
   has_many :ticket_types
+
+  validate :start_date_cannot_be_in_the_past
+
+  private
+
+  def start_date_cannot_be_in_the_past
+    if start_date.present? && start_date < Date.today
+      errors.add(:start_date, "can't be in the past")
+    end
+  end
+
+  def no_two_events_with_same_start_date
+    if start_date.present? && Event.where(event_venue_id: event_venue_id).select("start_date").include?(start_date)
+      errors.add(:start_date, "There's another event in that date")
+    end
+  end
 end

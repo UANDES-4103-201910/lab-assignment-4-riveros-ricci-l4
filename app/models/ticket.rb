791f6d2aab893683935b6e5d1a9f52f6549b4ad8
Pedro <priccis@gmail.com>
2019-04-03 11:02:24 -0300

    Validation prevent_creation_after_start_date Tickets

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,21 @@
 class Ticket < ApplicationRecord
   belongs_to :ticket_type
   belongs_to :order
+
+  before_save :prevent_creation_after_start_date, on: :create
+  before_save :prevent_selling_expired_ticket
+
+  private
+
+  def prevent_creation_after_start_date
+    associated_event = TicketType.where(id: ticket_type_id)
+    result = Event.select(:start_date).where(id: associated_event[0]['id']) unless associated_event.nil?
+    if !result.nil? && !result[0].nil? && Date.today > result[0]['start_date']
+      throw :abort
+    end
+  end
+
+  def prevent_selling_expired_ticket
+    puts 'TEST'
+  end
 end

9d1374151394ead85880e02b20168cb264225a07
Pedro <priccis@gmail.com>
2019-04-03 09:10:40 -0300

    Added required validations to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
   has_many :orders
+  validates :email, uniqueness: { case_sensitive: false }, presence: true,
+                    format: { with: /\A([^@\s]+)@((?:[a-z0-9-]+\.)+[a-z]{2,})\z/i }
+  validates :phone_number, length: { in: 9..12 }
+  validates :password, presence: true, length: { in: 6..20 },
+                       format: { with: /\A[a-zA-Z0-9]*\z//i }
+
+
 end

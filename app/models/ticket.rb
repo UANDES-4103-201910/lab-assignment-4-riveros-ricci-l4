99cf5977ef472e833d95dc84df4354fb2e9c2c49
erriveros <erriveros@miuandes.cl>
2019-04-04 22:44:55 -0300

    implemented prevent_sell

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,7 +3,7 @@ class Ticket < ApplicationRecord
   belongs_to :order
 
   before_save :prevent_creation_after_start_date, on: :create
-  before_save :prevent_selling_expired_ticket
+  before_save :prevent_selling_expired_ticket, on: :update
 
   private
 
@@ -15,5 +15,11 @@ class Ticket < ApplicationRecord
     end
   end
 
-  def prevent_selling_expired_ticket; end # Same as prevent creation?
+  def prevent_selling_expired_ticket
+    associated_event = TicketType.where(id: ticket_type_id)
+    result = !associated_event.nil? ? Event.select(:start_date).where(id: associated_event[0]['id']) : nil
+    if !result.nil? && !result[0].nil? && Date.today > result[0]['start_date']
+      throw :abort
+    end
+  end # Same as prevent creation?
 end
